f331a7829820ceebfd81443b265c756a055bbf0f
chrison.chai <chrison.chai@spreadtrum.com>
2019-11-28 10:00:03 +0800

    新增跨域配置

---
+++ src/main/java/cn/htz/chs/shiro/config/CORSConfiguration.java
@@ -0,0 +1,23 @@
+package cn.htz.chs.shiro.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 解决跨域问题springboot所需配置
+ */
+@Configuration
+public class CORSConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://htzshanghai.top")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                //设置是否允许跨域传cookie
+                .allowCredentials(true)
+                //设置缓存时间，减少重复响应
+                .maxAge(3600);
+    }
+}
